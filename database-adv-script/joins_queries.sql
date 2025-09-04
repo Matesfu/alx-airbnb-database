8449505f10acef2ca5c019e710a2a44007b9de6f
Matesfu <117965680+Matesfu@users.noreply.github.com>
2025-09-04 20:07:53 +0300

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -7,9 +7,11 @@ INNER JOIN Bookings ON Bookings.user_id = u.user_id
 SELECT Properties.property_id, Reviews.rating, reviews.comment
 FROM Properties
 LEFT JOIN Reviews ON Properties.property_id = Reviews.property_id
+ORDER BY Reviews.rating DESC
 
 -- a query using a FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user.
 SELECT Users.user_id, Users.first_name, bookings.booking_id
 FROM users
 FULL OUTER JOIN bookings ON Users.user_id = bookings.user_id
+ORDER BY Users.user_id
 
