2a58254da24ca4a82a28dddc022f9e285df7c982
Matesfu <117965680+Matesfu@users.noreply.github.com>
2025-09-04 20:03:12 +0300

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,14 +1,15 @@
 -- a query using an INNER JOIN to retrieve all bookings and the respective users who made those bookings.
-select Bookings.Booking_id, u.user_id
-from Users u
-inner join Bookings on Bookings.user_id = u.user_id
+SELECT Bookings.Booking_id, u.user_id
+FROM Users u
+INNER JOIN Bookings ON Bookings.user_id = u.user_id
 
 -- a query using aLEFT JOIN to retrieve all properties and their reviews, including properties that have no reviews.
-select Properties.property_id, Reviews.rating, reviews.comment
-from Properties
-left join Reviews on Properties.property_id = Reviews.property_id
+SELECT Properties.property_id, Reviews.rating, reviews.comment
+FROM Properties
+LEFT JOIN Reviews ON Properties.property_id = Reviews.property_id
 
 -- a query using a FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user.
-select Users.user_id, Users.first_name, bookings.booking_id
-from users
-FULL OUTER JOIN bookings on Users.user_id = bookings.user_id
+SELECT Users.user_id, Users.first_name, bookings.booking_id
+FROM users
+FULL OUTER JOIN bookings ON Users.user_id = bookings.user_id
+
